3d09020be99843c6a7504e315202869ec1bb7730
Thomas12321 <dreesthomas@yahoo.de>
2019-10-28 19:18:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-os7n3m
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-os7n3m)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Phone XL',
+    price: 799,
+    description: 'A large phone with one of the best screens',
+    size: '28mm'
+  },
+  {
+    name: 'Phone Mini',
+    price: 699,
+    description: 'A great phone with one of the best cameras',
+        size: '18mm'
+
+  },
+  {
+    name: 'Phone Standard',
+    price: 299,
+    description: '',
+        size: '24mm'
+
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
